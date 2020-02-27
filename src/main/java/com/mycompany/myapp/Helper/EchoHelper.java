971cb962c5439b72db5e91f14819da7b51e128d7
xhadl0811 <44574037+xhadl0811@users.noreply.github.com>
2020-02-27 21:58:39 +0900

    Update EchoHelper.java

--- src/main/java/com/mycompany/myapp/Helper/EchoHelper.java
+++ src/main/java/com/mycompany/myapp/Helper/EchoHelper.java
@@ -14,10 +14,10 @@ import com.mycompany.myapp.Mappers.userMapper;
 
 
 	
-public class EchoHelper extends TextWebSocketHandler {
+public class EchoHelper extends TextWebSocketHandler { //1:1채팅
 	 
 	 private Map<String,WebSocketSession> users = new HashMap<>(); // 유저들
-	 private Map<String,String> targets = new HashMap<>(); //상대유저가 나를보고있는지 확인
+	 private Map<String,String> targets = new HashMap<>(); //상대유저가 나를보고있는지 확인하기위함
 	 @Autowired
 	 userMapper uMapper;
 	 timeHelper tHelper = new timeHelper();
@@ -40,16 +40,15 @@ public class EchoHelper extends TextWebSocketHandler {
 		 String principal = (String)map.get("principal"); ///내 이메일
 		 String target = (String)map.get("target"); //상대 이메일
 		 String myNickname = uMapper.getNickname(principal);
-		 session.sendMessage(new TextMessage(myNickname+":"+message.getPayload()));			
-		 if(users.containsKey(target)&&targets.get(target).equals(principal))  //상대가 접속했는지 확인 후 , 상대가 나를보고있으면 메세지 전달 및 db저장 아니면 db저장 및 알림
-			users.get(target).sendMessage(new TextMessage(myNickname+":"+message.getPayload()));
+		 session.sendMessage(new TextMessage(myNickname+":"+message.getPayload()));//내 화면에 메세지 전달		
+		 if(users.containsKey(target)&&targets.get(target).equals(principal))  
+			users.get(target).sendMessage(new TextMessage(myNickname+":"+message.getPayload()));//상대 화면에 메세지 전달
 		uMapper.setChat(myNickname+":"+message.getPayload(),tHelper.getTime() , principal, target);
 	}
-			
+	 //상대가 접속했는지 확인 후 상대도 채팅페이지면 메세지 전달 , 아니면 db에만 저장	
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-
 			users.remove(session.getId());
 		}
 }
